ec0f49680760bb218d6ef06398335571619c2d29
VottusCode <VottusCode@users.noreply.github.com>
2020-12-10 10:32:26 +0000

    deploy: 1a72f2b45821ac2f63cd001b9b802811a235a7dc

---
+++ precache-manifest.0f99b42b6ba03f4b1c367de1820c641c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64e53e4317a67de53a24f0856b9dd12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bec9a4d16c49de1f01ee",
+    "url": "/static/js/2.2aba75d8.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.2aba75d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f20b4d26b711ed53a87",
+    "url": "/static/js/main.91022945.chunk.js"
+  },
+  {
+    "revision": "1714f7a52132622ced79",
+    "url": "/static/js/runtime-main.50dce5da.js"
+  }
+]);
